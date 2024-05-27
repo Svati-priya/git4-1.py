37bf2cadadb7c0a2ab4bf7f457cbf903f3a8b85f
Svati Priya <swatipriya98484@gmail.com>
2024-05-27 08:35:04 +0530

    first commit

---
+++ git4.py
@@ -0,0 +1,7 @@
+num1 = float(input("Enter the firts number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+average = (num1 + num2 + num3) / 3
+
+print("The average of the three number is:", average)
